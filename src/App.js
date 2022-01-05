774d85894760251c50c6f787e94e2e9b93784f59
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2022-01-05 14:30:06 +0000

    Made a simple wireframegit s

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function Box(props) {
   const mesh = useRef();
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-     <mesh {...props} ref={mesh}  scale={[1,2,2]}>
+     <mesh {...props} ref={mesh}  scale={[1,1,1]}>
         <boxGeometry args={[3, 3, 3]}   />
-        <meshStandardMaterial  color={"black"}  />
+        <meshStandardMaterial wireframe={true}  color={"black"}  />
      </mesh>
   );
 }
